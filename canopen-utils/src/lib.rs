e30b059c904103da3d85733480aed408eeac4799
Benjamin Schlegel <benjamin.schlegel@fsm.ag>
2022-08-22 08:23:00 +0200

    Basic Msg Structure and methods implemented

--- canopen-utils/src/lib.rs
+++ canopen-utils/src/lib.rs
@@ -1,3 +1,6 @@
+use std::ops::RangeInclusive;
+
+#[derive(Debug, PartialEq)]
 pub struct CANopenFrame {
     cob_id: u16,
     dlc: u8,
@@ -5,6 +8,11 @@ pub struct CANopenFrame {
 }
 
 impl CANopenFrame {
+    // 0 is fine because it is used from NMT Master to send NMT command to all nodes present
+    const NMT_ID_RANGE: RangeInclusive<u16> = 0..=0x07F;
+    const EMCY_ID_RANGE: RangeInclusive<u16> = 0x081..=0x0FF;
+    const SDO_ID_RANGE: RangeInclusive<u16> = 0x581..=0x67F;
+    const HB_ID_RANGE: RangeInclusive<u16> = 0x701..=0x77F;
     pub fn new(can_id: u32, dlc: u8, data: [u8; 8]) -> Option<Self> {
         if dlc > 8 {
             None
@@ -12,28 +20,26 @@ impl CANopenFrame {
             if can_id > 0x77F {
                 None
             } else {
-                let cob_id = u16::try_from(can_id);
-                if let Ok(id) = cob_id {
-                    Some(Self {
-                        cob_id: id,
-                        dlc,
-                        data,
-                    })
-                } else {
-                    None
-                }
+                // Safe because in this branch can_id is < 0x77F
+                let cob_id = can_id as u16;
+                Some(Self { cob_id, dlc, data })
             }
         }
     }
-    pub fn try_sdo_msg(&mut self) -> Option<Self> {
-        match self.cob_id {
-            0x581..=0x67F => Some(Self {
-                cob_id: self.cob_id,
-                dlc: self.dlc,
-                data: self.data,
-            }),
-            _ => None,
-        }
+    pub fn is_nmt_msg(&mut self) -> bool {
+        CANopenFrame::NMT_ID_RANGE.contains(&self.cob_id)
+    }
+    pub fn is_emcy_msg(&mut self) -> bool {
+        CANopenFrame::EMCY_ID_RANGE.contains(&self.cob_id)
+    }
+    pub fn is_sdo_msg(&mut self) -> bool {
+        CANopenFrame::SDO_ID_RANGE.contains(&self.cob_id)
+    }
+    pub fn is_hb_msg(&mut self) -> bool {
+        CANopenFrame::HB_ID_RANGE.contains(&self.cob_id)
+    }
+    pub fn get_data(&self) -> &[u8] {
+        &self.data
     }
 }
 
@@ -42,7 +48,45 @@ mod tests {
     use super::*;
     #[test]
     fn basic_api_test() {
-        let msg = CANopenFrame::new(0x604, 8, [0x43, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00])
-            .expect("msg not created");
+        assert_eq!(
+            None,
+            CANopenFrame::new(0x604, 9, [0x43, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00])
+        );
+        assert_eq!(
+            None,
+            CANopenFrame::new(0x780, 8, [0x43, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00])
+        );
+
+        let mut sdo_msg =
+            CANopenFrame::new(0x604, 8, [0x43, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00])
+                .expect("sdo msg not created");
+        assert_eq!(sdo_msg.is_sdo_msg(), true);
+        assert_eq!(sdo_msg.is_emcy_msg(), false);
+        assert_eq!(sdo_msg.is_hb_msg(), false);
+        assert_eq!(sdo_msg.is_nmt_msg(), false);
+
+        let mut nmt_msg =
+            CANopenFrame::new(0x000, 2, [0x7F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+                .expect("sdo msg not created");
+        assert_eq!(nmt_msg.is_sdo_msg(), false);
+        assert_eq!(nmt_msg.is_emcy_msg(), false);
+        assert_eq!(nmt_msg.is_hb_msg(), false);
+        assert_eq!(nmt_msg.is_nmt_msg(), true);
+
+        let mut emcy_msg =
+            CANopenFrame::new(0x086, 2, [0x7F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+                .expect("sdo msg not created");
+        assert_eq!(emcy_msg.is_sdo_msg(), false);
+        assert_eq!(emcy_msg.is_emcy_msg(), true);
+        assert_eq!(emcy_msg.is_hb_msg(), false);
+        assert_eq!(emcy_msg.is_nmt_msg(), false);
+
+        let mut hb_msg =
+            CANopenFrame::new(0x706, 2, [0x7F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+                .expect("hb msg not created");
+        assert_eq!(hb_msg.is_sdo_msg(), false);
+        assert_eq!(hb_msg.is_emcy_msg(), false);
+        assert_eq!(hb_msg.is_hb_msg(), true);
+        assert_eq!(hb_msg.is_nmt_msg(), false);
     }
 }
